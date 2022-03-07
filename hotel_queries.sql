a5f8fc90a5971d1984a7b8831be2b3c339732cc0
Matt Heerspink <mheerspink75@gmail.com>
2022-03-07 14:07:56 -0600

    updated query results comments

--- hotel_queries.sql
+++ hotel_queries.sql
@@ -6,7 +6,8 @@
 -- The results of the query in a comment under the query
 
 
--- 1. Write a query that returns a list of reservations that end in July 2023, including the name of the guest, the room number(s), and the reservation dates.
+-- 1. Write a query that returns a list of reservations that end in July 2023,
+-- including the name of the guest, the room number(s), and the reservation dates.
 
 SELECT firstname,
        lastname,
@@ -21,7 +22,6 @@ FROM   reservation rv
 WHERE  enddate BETWEEN '2023-07-01' AND '2023-07-31';
 
 -- Query Results:
--- Question 1=4 Rows, 
 -- +-----------+-----------+------------+------------+------------+
 -- | firstname | lastname  | roomnumber | startdate  | enddate    |
 -- +-----------+-----------+------------+------------+------------+
@@ -33,7 +33,8 @@ WHERE  enddate BETWEEN '2023-07-01' AND '2023-07-31';
 -- 4 rows in set (0.00 sec)
 
 
--- 2. Write a query that returns a list of all reservations for rooms with a jacuzzi, displaying the guest's name, the room number, and the dates of the reservation.
+-- 2. Write a query that returns a list of all reservations for rooms with a jacuzzi, 
+-- displaying the guest's name, the room number, and the dates of the reservation.
 
 SELECT firstname,
        lastname,
@@ -51,7 +52,6 @@ FROM   reservation rv
 WHERE  jacuzzi = 1;
 
 -- Query Results:
--- Question 2= 11 Rows, 
 -- +-----------+-----------+------------+------------+------------+---------+
 -- | firstname | lastname  | roomnumber | startdate  | enddate    | jacuzzi |
 -- +-----------+-----------+------------+------------+------------+---------+
@@ -70,7 +70,9 @@ WHERE  jacuzzi = 1;
 -- 11 rows in set (0.00 sec)
 
 
--- 3. Write a query that returns all the rooms reserved for a specific guest, including the guest's name, the room(s) reserved, the starting date of the reservation, and how many people were included in the reservation. (Choose a guest's name from the existing data.)
+-- 3. Write a query that returns all the rooms reserved for a specific guest, including the guest's name, the room(s) reserved, 
+-- the starting date of the reservation, and how many people were included in the reservation. 
+-- (Choose a guest's name from the existing data.)
 
 SELECT firstname,
        lastname,
@@ -88,7 +90,6 @@ WHERE  firstname = 'Mack'
        AND lastname = 'Simmer';
        
 -- Query Results:
--- Question 3= 4 Rows, 
 -- +-----------+----------+------------+--------+----------+------------+--------------+
 -- | firstname | lastname | roomnumber | adults | children | startdate  | total_guests |
 -- +-----------+----------+------------+--------+----------+------------+--------------+
@@ -100,7 +101,8 @@ WHERE  firstname = 'Mack'
 -- 4 rows in set (0.00 sec)
 
 
--- 4. Write a query that returns a list of rooms, reservation ID, and per-room cost for each reservation. The results should include all rooms, whether or not there is a reservation associated with the room.
+-- 4. Write a query that returns a list of rooms, reservation ID, and per-room cost for each reservation. 
+-- The results should include all rooms, whether or not there is a reservation associated with the room.
 
 SELECT roomnumber,
        reservationid,
@@ -114,8 +116,6 @@ FROM   room r
 ORDER  BY roomnumber;
 
 -- Query Results:
--- Question 4=27 rows, 
--- Returns 27 rows if Jerimiah Pendegrass is not deleted in the DML.
 -- +------------+---------------+-----------+-----------+
 -- | roomnumber | reservationid | baseprice | totalcost |
 -- +------------+---------------+-----------+-----------+
@@ -148,6 +148,41 @@ ORDER  BY roomnumber;
 -- +------------+---------------+-----------+-----------+
 -- 26 rows in set (0.00 sec)
 
+-- Returns 27 rows if 
+-- Jerimiah Pendegrass is not deleted in the DML.
+-- +------------+---------------+-----------+-----------+
+-- | roomnumber | reservationid | baseprice | totalcost |
+-- +------------+---------------+-----------+-----------+
+-- |        201 |             4 |    199.99 |    199.99 |
+-- |        202 |             7 |    174.99 |    349.98 |
+-- |        203 |             2 |    199.99 |    999.95 |
+-- |        203 |            21 |    199.99 |    399.98 |
+-- |        204 |            16 |    174.99 |    184.99 |
+-- |        205 |            15 |    174.99 |    699.96 |
+-- |        206 |            12 |    149.99 |    599.96 |
+-- |        206 |            23 |    149.99 |    449.97 |
+-- |        207 |            10 |    174.99 |    174.99 |
+-- |        208 |            13 |    149.99 |    599.96 |
+-- |        208 |            20 |    149.99 |    149.99 |
+-- |        301 |             9 |    199.99 |    799.96 |
+-- |        301 |            24 |    199.99 |    599.97 |
+-- |        302 |             6 |    174.99 |    924.95 |
+-- |        302 |            25 |    174.99 |    699.96 |
+-- |        303 |            18 |    199.99 |    199.99 |
+-- |        304 |             8 |    174.99 |    874.95 |
+-- |        304 |            14 |    174.99 |    184.99 |
+-- |        305 |             3 |    174.99 |    349.98 |
+-- |        305 |            19 |    174.99 |    349.98 |
+-- |        306 |          NULL |    149.99 |      NULL |
+-- |        307 |             5 |    174.99 |    524.97 |
+-- |        308 |             1 |    149.99 |    299.98 |
+-- |        401 |            11 |    399.99 |   1199.97 |
+-- |        401 |            17 |    399.99 |   1259.97 |
+-- |        401 |            22 |    399.99 |   1199.97 |
+-- |        402 |          NULL |    399.99 |      NULL |
+-- +------------+---------------+-----------+-----------+
+-- 27 rows in set (0.00 sec)
+
 
 -- 5. Write a query that returns all the rooms accommodating at least three guests and that are reserved on any date in April 2023.
 
@@ -166,8 +201,6 @@ WHERE  enddate BETWEEN '2023-04-01' AND '2023-04-30'
 ORDER  BY enddate;
 
 -- Query Results:
--- Question 5 (Let us know in group), 
--- Returns 2 rooms including room 304 if Jeremiah Pendergrass is not deleted in the DML...
 -- +---------------+------------+------------------+------------+------------+
 -- | reservationid | roomnumber | maximumoccupancy | startdate  | enddate    |
 -- +---------------+------------+------------------+------------+------------+
@@ -175,8 +208,18 @@ ORDER  BY enddate;
 -- +---------------+------------+------------------+------------+------------+
 -- 1 row in set (0.00 sec)
 
+-- Returns 2 rows if Jeremiah Pendergrass is not deleted in the DML...
+-- +---------------+------------+------------------+------------+------------+
+-- | reservationid | roomnumber | maximumoccupancy | startdate  | enddate    |
+-- +---------------+------------+------------------+------------+------------+
+-- |             8 |        304 |                4 | 2023-03-31 | 2023-04-05 |
+-- |             9 |        301 |                4 | 2023-04-09 | 2023-04-13 |
+-- +---------------+------------+------------------+------------+------------+
+-- 2 rows in set (0.00 sec)
 
--- 6. Write a query that returns a list of all guest names and the number of reservations per guest, sorted starting with the guest with the most reservations and then by the guest's last name.
+
+-- 6. Write a query that returns a list of all guest names and the number of reservations per guest, 
+-- sorted starting with the guest with the most reservations and then by the guest's last name.
 
 SELECT firstname,
        lastname,
@@ -190,8 +233,6 @@ ORDER  BY reservations_per_guest DESC,
           lastname;
           
 -- Query Results:
--- Question 6=12, 
--- Returns 12 rows if Jeremiah Pendergrass is not deleted in the DML...
 -- +-----------+-------------+------------------------+
 -- | firstname | lastname    | reservations_per_guest |
 -- +-----------+-------------+------------------------+
@@ -209,8 +250,28 @@ ORDER  BY reservations_per_guest DESC,
 -- +-----------+-------------+------------------------+
 -- 11 rows in set (0.00 sec)
 
+-- Returns 12 rows if
+-- Jeremiah Pendergrass is not deleted in the DML...
+-- +-----------+-------------+------------------------+
+-- | firstname | lastname    | reservations_per_guest |
+-- +-----------+-------------+------------------------+
+-- | Mack      | Simmer      |                      4 |
+-- | Bettyann  | Seery       |                      3 |
+-- | Duane     | Cullison    |                      2 |
+-- | Matt      | Heerspink   |                      2 |
+-- | Walter    | Holaway     |                      2 |
+-- | Aurore    | Lipton      |                      2 |
+-- | Maritza   | Tilton      |                      2 |
+-- | Joleen    | Tison       |                      2 |
+-- | Wilfred   | Vise        |                      2 |
+-- | Karie     | Yang        |                      2 |
+-- | Zachery   | Luechtefeld |                      1 |
+-- | Jeremiah  | Pendergrass |                      1 |
+-- +-----------+-------------+------------------------+
+-- 12 rows in set (0.00 sec)
 
--- 7. Write a query that displays the name, address, and phone number of a guest based on their phone number. (Choose a phone number from the existing data.)
+-- 7. Write a query that displays the name, address, and phone number of a guest based on their phone number. 
+-- (Choose a phone number from the existing data.)
 
 SELECT firstname,
        lastname,
@@ -220,7 +281,6 @@ FROM   guest
 WHERE  phone = '(446)351-6860';  
 
 -- Query Results:
--- Question 7=1
 -- +-----------+----------+---------------+----------------+
 -- | firstname | lastname | address       | phone          |
 -- +-----------+----------+---------------+----------------+
